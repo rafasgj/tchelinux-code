fdeca8d66ac4851f720b68ba9ab1e169ae28611f
Rafael Guterres Jeffman <rafasgj@gmail.com>
2018-12-13 10:13:49 -0200

    Fixed GIFImage to work with Python 3.

--- 2018/Pelotas/code/GIFImage.py
+++ 2018/Pelotas/code/GIFImage.py
@@ -1,44 +1,45 @@
-"""Based on GIFImage by Matthew Roe, fixed to work with Python 3 and PIL."""
+"""Based on GIFImage by Matthew Roe."""
 
 from PIL import Image
 import pygame
-from pygame.locals import *
 
-import time
+from time import time
+
 
 class GIFImage(object):
-    def __init__(self, filename):
+    """Define a sprite with its animation."""
+
+    def __init__(self, filename, pos=(0, 0)):
+        """Initialize a new sprite object."""
         self.filename = filename
         self.image = Image.open(filename)
         self.frames = []
-        self.get_frames()
-
+        self.__get_frames()
         self.cur = 0
-        self.ptime = time.time()
-
+        self.ptime = time()
         self.running = True
         self.breakpoint = len(self.frames)-1
         self.startpoint = 0
         self.reversed = False
+        self.pos = pos
 
     def get_rect(self):
-        return pygame.rect.Rect((0,0), self.image.size)
+        """Return sprite 'bounds' (x, y, w, h)."""
+        return pygame.rect.Rect(self.pos, self.image.size)
 
-    def get_frames(self):
+    def __get_frames(self):
         image = self.image
 
         pal = image.getpalette()
-        base_palette = []
-        for i in range(0, len(pal), 3):
-            rgb = pal[i:i+3]
-            base_palette.append(rgb)
+        palette = [[pal[j] for j in range(i, i+3)]
+                   for i in range(0, len(pal), 3)]
 
         all_tiles = []
         try:
             while 1:
                 if not image.tile:
                     image.seek(0)
-                if image.tile:
+                else:
                     all_tiles.append(image.tile[0][3][0])
                 image.seek(image.tell()+1)
         except EOFError:
@@ -48,12 +49,9 @@ class GIFImage(object):
 
         try:
             while 1:
-                try:
-                    duration = image.info["duration"]
-                except:
-                    duration = 100
+                duration = image.info.get("duration", 100)
 
-                duration *= .001 #convert to milliseconds!
+                duration *= .001  # convert to milliseconds!
                 cons = False
 
                 x0, y0, x1, y1 = (0, 0) + image.size
@@ -65,33 +63,15 @@ class GIFImage(object):
                 if len(tile) > 0:
                     x0, y0, x1, y1 = tile[0][1]
 
-                if all_tiles:
-                    if all_tiles in ((6,), (7,)):
-                        cons = True
-                        pal = image.getpalette()
-                        palette = []
-                        for i in range(0, len(pal), 3):
-                            rgb = pal[i:i+3]
-                            palette.append(rgb)
-                    elif all_tiles in ((7, 8), (8, 7)):
-                        pal = image.getpalette()
-                        palette = []
-                        for i in range(0, len(pal), 3):
-                            rgb = pal[i:i+3]
-                            palette.append(rgb)
-                    else:
-                        palette = base_palette
-                else:
-                    palette = base_palette
-
-                pi = pygame.image.fromstring(image.tobytes(), image.size, image.mode)
+                pi = pygame.image.fromstring(image.tobytes(),
+                                             image.size, image.mode)
                 pi.set_palette(palette)
                 if "transparency" in image.info:
                     pi.set_colorkey(image.info["transparency"])
-                pi2 = pygame.Surface(image.size, SRCALPHA)
+                pi2 = pygame.Surface(image.size, pygame.SRCALPHA)
                 if cons:
                     for i in self.frames:
-                        pi2.blit(i[0], (0,0))
+                        pi2.blit(i[0], (0, 0))
                 pi2.blit(pi, (x0, y0), (x0, y0, x1-x0, y1-y0))
 
                 self.frames.append([pi2, duration])
@@ -99,9 +79,10 @@ class GIFImage(object):
         except EOFError:
             pass
 
-    def draw(self, screen, pos=None):
+    def draw(self, screen, pos=None, loop=True):
+        """Draw the sprite to the given screen."""
         if self.running:
-            if time.time() - self.ptime > self.frames[self.cur][1]:
+            if time() - self.ptime > self.frames[self.cur][1]:
                 if self.reversed:
                     self.cur -= 1
                     if self.cur < self.startpoint:
@@ -110,93 +91,67 @@ class GIFImage(object):
                     self.cur += 1
                     if self.cur > self.breakpoint:
                         self.cur = self.startpoint
+                        if not loop:
+                            self.running = False
+                            return
 
-                self.ptime = time.time()
-
+                self.ptime = time()
         screen.blit(self.frames[self.cur][0], pos)
 
-    def seek(self, num):
-        self.cur = num
-        if self.cur < 0:
-            self.cur = 0
-        if self.cur >= len(self.frames):
-            self.cur = len(self.frames)-1
+    def seek(self, frame):
+        """Set next frame to the given one."""
+        assert frame >= 0 and frame <= len(self.frames)
+        self.cur = frame
 
-    def set_bounds(self, start, end):
-        if start < 0:
-            start = 0
-        if start >= len(self.frames):
-            start = len(self.frames) - 1
-        if end < 0:
-            end = 0
-        if end >= len(self.frames):
-            end = len(self.frames) - 1
+    def set_frames(self, start, end):
+        """Set frame range to use."""
+        assert start >= 0 and start < len(self.frames) - 1
+        assert end >= 0 and end < len(self.frames) - 1
         if end < start:
-            end = start
+            start, end = end, start
+            self.reversed = True
+        else:
+            self.reversed = False
         self.startpoint = start
         self.breakpoint = end
 
     def pause(self):
+        """Stop animation."""
         self.running = False
 
     def play(self):
+        """Start animation."""
         self.running = True
 
-    def rewind(self):
-        self.seek(0)
-    def fastforward(self):
+    def restart(self):
+        """Restart at first frame."""
+        self.seek(self.startpoint)
+
+    def __fastforward(self):
         self.seek(self.length()-1)
 
     def get_height(self):
+        """Get image height."""
         return self.image.size[1]
+
     def get_width(self):
+        """Get image width."""
         return self.image.size[0]
+
     def get_size(self):
+        """Get image size."""
         return self.image.size
+
     def length(self):
+        """Get number of frames."""
         return len(self.frames)
+
     def reverse(self):
+        """Play reversed."""
         self.reversed = not self.reversed
+
     def reset(self):
+        """Reset animations."""
         self.cur = 0
         self.ptime = time.time()
         self.reversed = False
-
-    def copy(self):
-        new = GIFImage(self.filename)
-        new.running = self.running
-        new.breakpoint = self.breakpoint
-        new.startpoint = self.startpoint
-        new.cur = self.cur
-        new.ptime = self.ptime
-        new.reversed = self.reversed
-        return new
-
-##def main():
-##    pygame.init()
-##    screen = pygame.display.set_mode((640, 480))
-##
-##    hulk = GIFImage("hulk.gif")
-##    football = GIFImage("football.gif")
-##    hulk2 = hulk.copy()
-##    hulk2.reverse()
-##    hulk3 = hulk.copy()
-##    hulk3.set_bounds(0, 2)
-##    spiderman = GIFImage("spiderman7.gif")
-##
-##    while 1:
-##        for event in pygame.event.get():
-##            if event.type == QUIT:
-##                pygame.quit()
-##                return
-##
-##        screen.fill((255,255,255))
-##        hulk.render(screen, (50, 0))
-##        hulk2.render(screen, (50, 150))
-##        hulk3.render(screen, (50, 300))
-##        football.render(screen, (200, 50))
-##        spiderman.render(screen, (200, 150))
-##        pygame.display.flip()
-##
-##if __name__ == "__main__":
-##    main()
